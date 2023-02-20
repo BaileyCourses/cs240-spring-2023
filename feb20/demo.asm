a4e499d7b20ed24ec6f556dcbb1405ccb0024a6d
Mark Bailey <mwbmwb@users.noreply.github.com>
2023-02-20 14:20:21 -0500

    Feb 20

---
+++ feb20/demo.asm
@@ -0,0 +1,27 @@
+;;; Professor Bailey
+;;; Spring 2023
+
+include cs240.inc
+include lib.inc
+	
+DOSEXIT = 4C00h
+DOS = 21h
+
+.8086
+
+.data
+	
+.code
+
+;;; Entry point for the program
+
+main PROC
+	mov	ax, @data	; Setup the data segment
+	mov	ds, ax
+
+	;; Start of DOS program...
+
+	mov	ax, DOSEXIT	; Signal DOS that we are done
+	int	DOS
+main ENDP
+END main
