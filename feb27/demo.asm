a914911d18b2323c4ce0708b22f0ada9884c5569
Mark Bailey <mwbmwb@users.noreply.github.com>
2023-03-08 12:09:27 -0500

    Feb 27

---
+++ feb27/demo.asm
@@ -0,0 +1,61 @@
+;;; Professor Bailey
+;;; Spring 2023
+
+include cs240.inc
+include lib.inc
+	
+DOSEXIT = 4C00h
+DOS = 21h
+BIOS = 10h
+
+TIMER = 1Ch
+
+.8086
+
+.data
+	
+.code
+
+.data
+bufblock	LABEL	BYTE
+startcan	BYTE	"*****************"
+buffer	BYTE	10 DUP("-")
+endcan		BYTE	"**********"
+blocksize = $ - bufblock
+.code
+	
+WriteBlock PROC
+	push	dx
+	push	cx
+
+	mov	dx, OFFSET bufblock
+	mov	cx, blocksize
+	call	WriteBuffer
+	call	NewLine
+
+	pop	cx
+	pop	dx
+	ret
+WriteBlock ENDP
+
+main PROC
+	mov	ax, @data	; Setup the data segment
+	mov	ds, ax
+
+	call	WriteBlock
+
+	mWrite	"Enter your name: "
+	mov	dx, OFFSET buffer
+	push	dx
+	mov	dx, LENGTHOF buffer
+	push	dx
+	call	SafeRead
+	add	sp, 4
+	call	NewLine
+
+	call	WriteBlock
+
+	mov	ax, DOSEXIT	; Signal DOS that we are done
+	int	DOS
+main ENDP
+END main
